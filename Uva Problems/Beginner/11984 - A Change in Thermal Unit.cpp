883c32dea910d27c83af8beeeacb22a5866e9acc
Raihan Sikdar (Afridi) <raihansikdar10@gmail.com>
2022-08-27 14:26:54 +0600

    Update 11984 - A Change in Thermal Unit.cpp

--- Uva Problems/Beginner/11984 - A Change in Thermal Unit.cpp
+++ Uva Problems/Beginner/11984 - A Change in Thermal Unit.cpp
@@ -1,4 +1,3 @@
-/*
 #include<bits/stdc++.h>
 using namespace std;
 typedef		long long       ll;
@@ -33,25 +32,4 @@ int main()
     return 0;
 }
 
-*/
 
-
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int a,b,c;
-    while(scanf("%d%d%d",&a,&b,&c)==3)
-    {
-        if(a==0||b==0||c==0)
-            break;
-        if (a<=0 || b<=0 ||c<=0)
-            puts("wrong");
-       else if(a*a==b*b + c*c || b*b==a*a + c*c || c*c==a*a + b*b)
-           puts("right");
-
-        else puts("wrong");
-
-
-}
-}
