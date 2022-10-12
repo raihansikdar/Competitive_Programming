f2e5c6f44b9193dbf127e5c1717fe3162df14ff6
Raihan Sikdar (Afridi) <raihansikdar10@gmail.com>
2022-10-13 00:04:37 +0600

    Contests Code

---
+++ Codechef/Contests Code/START60D/1.Tax in Chefland.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     p(a)           printf("%d\n",a);
+#define		nn             "\n"
+
+void solve()
+{
+       ll n;
+       S(n);
+
+       if(n>100) 
+       {
+           p(n-10);
+       }
+       else p(n);
+}
+
+int main()
+{ 
+	freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
