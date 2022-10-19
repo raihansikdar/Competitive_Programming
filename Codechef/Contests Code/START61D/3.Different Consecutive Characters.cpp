4cd24fd0c7399b3a471d8a02f164aa84ac5faf39
Raihan Sikdar (Afridi) <raihansikdar10@gmail.com>
2022-10-20 00:16:51 +0600

    Contests Code START61D

---
+++ Codechef/Contests Code/START61D/3.Different Consecutive Characters.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     Sf(a)           scanf("%f",&a);
+#define     SSf(a,b)        scanf("%f %f",&a,&b);
+#define     SSSf(a,b,c)     scanf("%f %f %f",&a,&b,&c);
+#define     SSSSf(a,b,c,d)     scanf("%f %f %f %f",&a,&b,&c,&d);
+#define     p(a)           printf("%lld\n",a);
+#define		nn             "\n"
+
+
+void solve()
+{
+   ll n,i,c=0;
+   S(n);
+   string st;
+   cin>>st;
+
+   for(i=0;i<n;i++)
+   {
+     if(st[i]==st[i+1])
+        c++;
+   }
+    cout<<c<<endl;
+}
+
+int main()
+{ 
+	freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
---
+++ Codechef/Contests Code/START61D/4.Minimise LCS.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     Sf(a)           scanf("%f",&a);
+#define     SSf(a,b)        scanf("%f %f",&a,&b);
+#define     SSSf(a,b,c)     scanf("%f %f %f",&a,&b,&c);
+#define     SSSSf(a,b,c,d)     scanf("%f %f %f %f",&a,&b,&c,&d);
+#define     p(a)           printf("%d\n",a);
+#define		nn             "\n"
+
+
+void solve()
+{
+    int n, s = 0;
+    string a, b;
+    cin >> n >> a >> b;
+    map<char, int> P, Q;
+    for (char i : a)
+        P[i]++;
+    for (char i : b)
+        Q[i]++;
+    for (char i = 'a'; i <= 'z'; ++i)
+        s = max(s, min(P[i], Q[i]));
+    cout << s << endl;
+}
+
+int main()
+{ 
+	freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
