edcb69dcf1cb22c9d00151fede2eecf49b804871
Raihan Sikdar (Afridi) <raihansikdar10@gmail.com>
2022-11-07 15:20:09 +0600

    Contests Code START63D

---
+++ Codechef/Contests Code/START63D/2.Packing Books.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     Sf(a)           scanf("%f",&a);
+#define     SSf(a,b)        scanf("%f %f",&a,&b);
+#define     SSSf(a,b,c)     scanf("%f %f %f",&a,&b,&c);
+#define     SSSSf(a,b,c,d)     scanf("%f %f %f %f",&a,&b,&c,&d);
+#define     p(a)           printf("%d\n",a);
+#define		nn             "\n"
+
+#define     For(i,x,y)        for( int i =int(x); i<int(y); i++)
+
+void solve()
+{
+        ll x,y,z;
+        SSS(x,y,z);
+
+        if(z >= y)
+        {
+            p(x);
+        }
+
+       else
+        {
+            if(y%z==0){
+                p(x*(y/z));
+            }
+            else
+                p(x*(y/z+1));
+        }
+}
+
+int main()
+{ 
+	freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
