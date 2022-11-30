f596a16007ff7e8a7143ad0c5aebd66ac3542934
Raihan Sikdar (Afridi) <raihansikdar10@gmail.com>
2022-11-30 21:12:06 +0600

    Contests Code START67D

---
+++ Codechef/Contests Code/START67D/1.Tour of King.cpp
@@ -0,0 +1,72 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<iterator>
+#include<cmath>
+#include<cstring>
+#include<limits>
+using namespace std;
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     Sf(a)           scanf("%f",&a);
+#define     SSf(a,b)        scanf("%f %f",&a,&b);
+#define     SSSf(a,b,c)     scanf("%f %f %f",&a,&b,&c);
+#define     SSSSf(a,b,c,d)     scanf("%f %f %f %f",&a,&b,&c,&d);
+#define     p(a)           printf("%d\n",a);
+#define		nn             "\n"
+
+#define     For(i,x,y)        for( int i =int(x); i<int(y); i++)
+
+void solve()
+{
+    ll a,b;
+    SS(a,b);
+
+    ll ans = a*5 + b*7;
+    p(ans);
+}
+
+int main()
+{ 
+	freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
+
+/*
+--------|
+INPUT:  |
+--------|
+4
+4 8
+2 13
+14 5
+8 8
+
+--------|
+OUTPUT: |
+--------|
+76
+101
+105
+96
+
+*/
---
+++ Codechef/Contests Code/START67D/3.Counting Problem.cpp
@@ -0,0 +1,86 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<iterator>
+#include<cmath>
+#include<cstring>
+#include<limits>
+using namespace std;
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     Sf(a)           scanf("%f",&a);
+#define     SSf(a,b)        scanf("%f %f",&a,&b);
+#define     SSSf(a,b,c)     scanf("%f %f %f",&a,&b,&c);
+#define     SSSSf(a,b,c,d)     scanf("%f %f %f %f",&a,&b,&c,&d);
+#define     p(a)           printf("%d\n",a);
+#define		nn             "\n"
+
+#define     For(i,x,y)        for( int i =int(x); i<int(y); i++)
+
+void solve()
+{
+  ll n,c=0;
+  S(n);
+  ll arr[n];
+
+  For(i,0,n)
+  {
+    S(arr[i]);
+
+    if(arr[i]%2!=0)
+      c++;
+  }
+
+  cout<<((c==0 || c%2!=0) ? "NO" : "YES")<<endl;
+
+
+}
+
+int main()
+{ 
+	  freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
+
+/*
+--------|
+INPUT:  |
+--------|
+4
+4
+1 1 2 2
+6
+1 2 4 6 8 10
+2
+3 5
+3
+1 3 5
+
+--------|
+OUTPUT: |
+--------|
+YES
+NO
+YES
+NO
+
+*/
