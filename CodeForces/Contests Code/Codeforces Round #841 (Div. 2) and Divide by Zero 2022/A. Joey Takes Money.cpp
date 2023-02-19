30438819258f13bdd7609e1a1ba35231f3aa8477
Raihan Sikdar (Afridi) <raihansikdar10@gmail.com>
2023-02-19 17:54:15 +0600

    Contest Code

---
+++ CodeForces/Contests Code/Codeforces Round #841 (Div. 2) and Divide by Zero 2022/A. Joey Takes Money.cpp
@@ -0,0 +1,84 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<iterator>
+#include<cmath>
+#include<cstring>
+#include<limits>
+using namespace std;
+typedef     long long      ll;
+
+#define     S(a)           scanf("%lld",&a);
+#define     SS(a,b)        scanf("%lld %lld",&a,&b);
+#define     SSS(a,b,c)     scanf("%lld %lld %lld",&a,&b,&c);
+#define     SSSS(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+#define     Sf(a)           scanf("%f",&a);
+#define     SSf(a,b)        scanf("%f %f",&a,&b);
+#define     SSSf(a,b,c)     scanf("%f %f %f",&a,&b,&c);
+#define     SSSSf(a,b,c,d)     scanf("%f %f %f %f",&a,&b,&c,&d);
+#define     p(a)           printf("%d\n",a);
+#define		nn             "\n"
+
+#define     For(i,x,y)        for( int i =int(x); i<int(y); i++)
+
+void solve()
+{
+    ll n;
+    cin>> n;
+    ll arr[n];
+    ll count = 1;
+
+    For(i,0,n)
+    {
+        cin>>arr[n];
+        count = count*arr[n];
+    }
+
+    count = count + (n-1);
+    cout<<count*2022<<endl;
+  
+}
+
+int main()
+{ 
+	freopen( "input.txt","r", stdin);
+    freopen( "my_output.txt","w", stdout);
+
+    ll t;
+    S(t);
+    while(t--)
+    {
+    	solve();
+    }
+
+    
+    return 0;
+
+
+}
+
+/*
+--------|
+INPUT:  |
+--------|
+3
+3
+2 3 2
+2
+1 3
+3
+1000000 1000000 1
+
+
+--------|
+OUTPUT: |
+--------|
+28308
+8088
+2022000000004044
+
+
+*/
